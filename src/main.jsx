accb51df5e3306079bd2dfec4520fa372f5498fd
Raj Su <rajsu@example.com>
2026-06-01 23:34:04 +0530

    feat: wrap App with Redux Provider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// Entry point - wraps App with Redux Provider
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* Redux Provider - makes store available to all components */}
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
